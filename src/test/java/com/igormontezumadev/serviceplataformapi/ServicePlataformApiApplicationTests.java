a2561c71400fa98d0834327447e27d3edc873e81
IgorMontezuma20 <igorminranda17@gmail.com>
2024-07-07 20:03:04 -0300

    feat: Criando as estruturas da entidade User

---
+++ src/test/java/com/igormontezumadev/serviceplataformapi/ServicePlataformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igormontezumadev.serviceplataformapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicePlataformApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
